fac87ed9f2249fc50686d7026e2ef8990e853316
zzyzzy <zzyzigy@gmail.com>
2025-02-17 16:23:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 미세먼지 스프링부트앱
+### 에어코리아 미세먼지 측정 api
++ https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=&returnType=json&numOfRows=200&pageNo=1&sidoName=%EC%84%9C%EC%9A%B8&ver=1.0
---
+++ src/test/java/com/example/zzyzzy/airquality/AirQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zzyzzy.airquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
