28b03b00e5fe354a7796a019d4db66432383f030
zzyzzy <zzyzigy@gmail.com>
2025-02-18 10:16:36 +0900

    미세먼지 정보 가져오는 메서드 작성 : getAirQualityDataBasic getAirQualityDataBasic에 대한 테스트 클래스 작성

---
+++ src/test/java/com/example/zzyzzy/airquality/AirQualityServiceTest.java
@@ -0,0 +1,38 @@
+package com.example.zzyzzy.airquality;
+
+import com.example.zzyzzy.airquality.service.AirQualityService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AirQualityServiceTest {
+
+    private AirQualityService airQualityService;
+
+    @BeforeEach
+    public void setUp() {
+        airQualityService = new AirQualityService();
+    }
+
+    @Test
+    @DisplayName("testAirQualityServiceBasic")
+    public void testAirQualityServiceBasic() throws IOException {
+        // 테스트 실행 (메서드 호출)
+        String result = airQualityService.getAirQualityDataBasic();
+
+        // 결과 검증
+        assertNotNull(result);  // 응답이 Null인지 여부 확인
+        assertFalse(result.isEmpty());  // 응답이 비었는지 여부 확인
+
+        // JSON응답이 유효한지 여부 확인
+        // 즉, 올바른 JSON 형태의 데이터인지 확인
+        assertDoesNotThrow(()
+                -> new ObjectMapper().readTree(result));
+    }
+
+}
